6b92005f34d01baca84a838f59be99ac097a0875
Ralph Hirtt <ralph.hirtt@cyber.dlh.lu>
2026-06-15 21:25:32 +0200

    subfinder added

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-subfinder -silent -o $1.tmp -oI -active -d $1 |awk -F, '{print $1}'
-cat $1.tmp |awk -F, -v OFS=',' '{print $1,$2}' >$1.txt
+subfinder -silent -o $1.txt -oI -active -d $1 
