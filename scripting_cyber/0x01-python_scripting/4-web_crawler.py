e45b193ff43b7493c36ddfd7e2a354fc7ba940f4
Ralph Hirtt <ralph.hirtt@cyber.dlh.lu>
2026-07-06 22:46:07 +0200

    web crawler

--- scripting_cyber/0x01-python_scripting/4-web_crawler.py
+++ scripting_cyber/0x01-python_scripting/4-web_crawler.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
 import requests
-import urllib
-from bs4 import BeautifulSoup
-
-#!/usr/bin/env python3
-import requests
-import urllib
+from urllib.parse import urljoin, urlparse
 from bs4 import BeautifulSoup
 
 def crawl_website(start_url, max_depth=2):
@@ -19,9 +14,8 @@ def crawl_website(start_url, max_depth=2):
       links = soup.find_all("a")
       for link in links:
         try:
-          url=urllib.parse.urljoin(start_url,link['href'])
-          #print(url)
-          if urllib.parse.urlparse(start_url).netloc == urllib.parse.urlparse(url).netloc:
+          url=urljoin(start_url,link['href'])
+          if urlparse(start_url).netloc == urllib.parse.urlparse(url).netloc:
             res = crawl_website(url, max_depth)
             result_set =result_set.union(res)
         except Exception:
